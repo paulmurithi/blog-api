1deab50677a7e6c0830cb27c8f3336dc92650c15
unknown <murithipaul1@gmail.com>
2020-03-10 02:48:17 -0700

    added api routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::ApiResource('categories', 'CategoryController');
+Route::ApiResource('posts', 'PostController');
+Route::ApiResource('comments', 'CommentController');
+Route::ApiResource('replies', 'ReplyController');
