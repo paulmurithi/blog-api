c581ca51c2eb123ac4459d1bc1049c684c18557c
unknown <murithipaul1@gmail.com>
2020-03-10 04:29:37 -0700

    added body column to the replies table

--- database/migrations/2020_03_10_103040_create_replies_table.php
+++ database/migrations/2020_03_10_103040_create_replies_table.php
@@ -17,8 +17,9 @@ class CreateRepliesTable extends Migration
             $table->id();
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('comment_id');
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');;
-            $table->foreign('comment_id')->references('id')->on('comments')->onDelete('cascade');;
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('comment_id')->references('id')->on('comments')->onDelete('cascade');
+            $table->string('body',500);
             $table->timestamps();
         });
     }
